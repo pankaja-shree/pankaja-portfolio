a92fba5473b971507d2fd046702b8fc0ac199856
Pankajashree <pankajashree@gmail.com>
2018-03-05 22:14:25 +0530

    add footer links

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+<footer>
+     <nav>
+      <ul>
+      <li><a href="https://github.com/pankaja-shree/" target="_blank">Github</a></li>
+      <li><a href="https://in.linkedin.com/in/pankajashree-r-0a2a5167">LinkedIn</a></li>
+      <li><a href="https://twitter.com/pankajashree">Twitter</a></li>
+      <li><a href="https://medium.com/@pankajashree">Blog</a></li>
+      </ul>
+    </nav>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer'
+}
+</script>
+
+<style>
+footer {
+  margin: 0;
+  height: 56px;
+  padding: 2%;
+}
+
+footer nav ul {
+  display: flex;
+  justify-content: center;
+}
+
+footer a {
+    color: #000;
+    text-decoration: none;
+    transition: all 0.5s ease;
+}
+
+footer a:hover {
+    border-bottom: 2px solid #000;
+    cursor: pointer;
+}
+</style>
