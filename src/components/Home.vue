7b41d115c1a4fce26f24dd30e194b5f2f6cb4b2c
Pankajashree <pankajashree@gmail.com>
2018-03-05 22:14:44 +0530

    edit intro para

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@ export default {
   name: 'home',
   data () {
     return {
-      introPara: "I'm Pankajashree, a web developer from Mysore. I have been building mobile first, progressive web applications that is accessible to all using Javascript and Python since 2 years."
+      introPara: "I'm Pankajashree, researcher and web developer from Mysore. I have been building mobile first, progressive web applications that is accessible to all using Javascript and Python since 2 years."
     }
   }
 }
@@ -19,7 +19,7 @@ export default {
 <style>
 p {
   padding: 8%;
-  font-size: 1.5em;
+  font-size: 1.3em;
   line-height: 2;
 }
 
